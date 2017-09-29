bd620e6aaaa036aa02c0fbca10aa5636b48f5467
Nicolle Cysneiros <nicolle.cysneiros@gmail.com>
2017-09-28 23:55:35 -0300

    Creates blog post model

--- customblog/blog/models.py
+++ customblog/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{title} - by {first_name} {last_name}".format(
+            title=self.title,
+            first_name=self.author.first_name,
+            last_name=self.author.last_name
+        )
